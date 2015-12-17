f1ac6a76b259991cc59953cafbf51f6e98198b4b
cc <zheruicao@ZheruideMacBook-Pro.local>
2015-12-18 00:29:26 +0800

    引流 电商网站

--- app/src/main/java/com/elinc/im/haer/ui/RentBook.java
+++ app/src/main/java/com/elinc/im/haer/ui/RentBook.java
@@ -3,6 +3,7 @@ package com.elinc.im.haer.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -35,7 +36,8 @@ import cn.bmob.v3.listener.UpdateListener;
 public class RentBook extends ActivityBase {
 
     private TextView apply;
-    private TextView done;
+    private String title;
+    private TextView done,jd;
     private User u=new User();
     private Book book=new Book();
     private String available;
@@ -71,6 +73,8 @@ public class RentBook extends ActivityBase {
         book_cover=(ImageView)findViewById(R.id.book_cover);
         apply= (TextView) findViewById(R.id.btn_confirm_apply);
         done= (TextView) findViewById(R.id.btn_done);
+        jd=(TextView)findViewById(R.id.btn_jd);
+
         u=BmobUser.getCurrentUser(RentBook.this, User.class);
         book.setObjectId(getIntent().getStringExtra("book"));
         available=getIntent().getStringExtra("available");
@@ -87,35 +91,44 @@ public class RentBook extends ActivityBase {
         q.getObject(RentBook.this, getIntent().getStringExtra("book"), new GetListener<Book>() {
             @Override
             public void onSuccess(Book bb) {
-                String avatar=bb.getBookAvatar();
+                String avatar = bb.getBookAvatar();
                 if (avatar != null && !avatar.equals("")) {//加载头像-为了不每次都加载头像
                     ImageLoader.getInstance().displayImage(avatar, book_cover, ImageLoadOptions.getOptions(), animateFirstListener);
                 } else {
                     book_cover.setImageResource(R.drawable.no_cover);
                 }
 
-                if(bb.getOwner().getUsername().equals(u.getUsername())){
+
+                title=bb.getTitle();
+                if (bb.getAvailable() == false) {
                     apply.setVisibility(View.GONE);
+                    jd.setVisibility(View.VISIBLE);
+                }
+                if (bb.getOwner().getUsername().equals(u.getUsername())) {
+                    apply.setVisibility(View.GONE);
+                    jd.setVisibility(View.GONE);
                     done.setVisibility(View.VISIBLE);
-                    author_name= bb.getOwner().getUsername();
+                    author_name = bb.getOwner().getUsername();
 
                 }
+
             }
 
             @Override
             public void onFailure(int i, String s) {
-                Tool.alert(RentBook.this,"没有网络");
+                Tool.alert(RentBook.this, "没有网络");
             }
         });
         done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                u=BmobUser.getCurrentUser(RentBook.this, User.class);
+                u = BmobUser.getCurrentUser(RentBook.this, User.class);
                 book.setAvailable(true);
                 book.update(RentBook.this, getIntent().getStringExtra("book"), new UpdateListener() {
                     @Override
                     public void onSuccess() {
                         Tool.alert(RentBook.this, "OK，可以等待下一个借书的人了");
+                        refresh();
                     }
 
                     @Override
@@ -137,6 +150,7 @@ public class RentBook extends ActivityBase {
                     public void onSuccess() {
 
                         Tool.alert(RentBook.this, "done");
+                        refresh();
                     }
 
                     @Override
@@ -148,6 +162,17 @@ public class RentBook extends ActivityBase {
             } else {
                 Tool.alert(RentBook.this, "不好意思，这本书的拥有者现在还没有读完，先等等吧！如果急的话可以去书店买本新的");
             }
+            }
+        });
+        jd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //jump to jd.com
+                //http://search.jd.com/Search?keyword=hello&enc=utf-8
+                Intent it = new Intent(Intent.ACTION_VIEW, Uri.parse("http://search.jd.com/Search?keyword="+title+"&enc=utf-8"));
+                //it.setClassName("com.android.browser", "com.android.browser.BrowserActivity");
+                RentBook.this.startActivity(it);
+
             }
         });
     }
@@ -189,6 +214,44 @@ public class RentBook extends ActivityBase {
                 }
             }
         }
+    }
+    private void refresh(){
+        u=BmobUser.getCurrentUser(RentBook.this, User.class);
+        book.setObjectId(getIntent().getStringExtra("book"));
+        available=getIntent().getStringExtra("available");
+        // Tool.alert(RentBook.this, book.getObjectId());
+        img= (ImageView) findViewById(R.id.img_friend_avatar);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openUserDetail();
+            }
+        });
+        BmobQuery<Book> q=new BmobQuery<Book>();
+        q.include("owner");
+        q.getObject(RentBook.this, getIntent().getStringExtra("book"), new GetListener<Book>() {
+            @Override
+            public void onSuccess(Book bb) {
+                String avatar=bb.getBookAvatar();
+                if (avatar != null && !avatar.equals("")) {//加载头像-为了不每次都加载头像
+                    ImageLoader.getInstance().displayImage(avatar, book_cover, ImageLoadOptions.getOptions(), animateFirstListener);
+                } else {
+                    book_cover.setImageResource(R.drawable.no_cover);
+                }
+
+                if(bb.getOwner().getUsername().equals(u.getUsername())){
+                    apply.setVisibility(View.GONE);
+                    done.setVisibility(View.VISIBLE);
+                    author_name= bb.getOwner().getUsername();
+
+                }
+            }
+
+            @Override
+            public void onFailure(int i, String s) {
+                Tool.alert(RentBook.this,"没有网络");
+            }
+        });
     }
 
 }
